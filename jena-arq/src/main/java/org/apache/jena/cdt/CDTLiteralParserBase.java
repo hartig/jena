83ba61f4c6dc12679eed520cf059c78dbf368ec3
Olaf Hartig <olaf.hartig@liu.se>
2024-02-02 17:57:23 +0100

    initial test of how to share the scope of bnode creation across all cdt:List literals within a Turtle file

--- jena-arq/src/main/java/org/apache/jena/cdt/CDTLiteralParserBase.java
+++ jena-arq/src/main/java/org/apache/jena/cdt/CDTLiteralParserBase.java
@@ -8,6 +8,16 @@ import org.apache.jena.riot.lang.extra.LangParserBase;
 public class CDTLiteralParserBase extends LangParserBase
 {
 	@Override
+	protected Node createBNode(String label, int line, int column) {
+		final Node b = super.createBNode(label, line, column);
+System.out.println("createBNode(" + label + ") " + b.toString() + "  -- profile type: " + profile.getClass().getName() );
+//TODO: first step, the parser profile used for the parsing of the TTL file is still not the CDT-aware one :-(
+//		second step, think whether the profile used here should not be the CDT-aware one but just the standard one
+//		...or perhaps not? think about recursive parsing of CDT literals -- the question is about bnode identifiers
+//		inside a CDT literal that is inside another CDT literal
+		return b;
+	}
+	@Override
 	protected Node createLiteral( final String lex, final String langTag, final String datatypeURI, final int line, final int column ) {
 		if ( CompositeDatatypeList.uri.equals(datatypeURI) ) {
 			final LiteralLabel lit = new LiteralLabelForList(lex);
